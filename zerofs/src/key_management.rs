d2c10708e9650c9134babe191d24f78567f91b2e
Pierre Barre <pierre@barre.sh>
2025-09-01 14:36:33 +0200

    Remove `await_durable: true` in password change, this will hang, and our flush after this call makes it durable

--- zerofs/src/key_management.rs
+++ zerofs/src/key_management.rs
@@ -219,7 +219,7 @@ pub async fn change_encryption_password(
         &serialized,
         &slatedb::config::PutOptions::default(),
         &slatedb::config::WriteOptions {
-            await_durable: true,
+            await_durable: false,
         },
     )
     .await?;
