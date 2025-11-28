72d9485c616818215961ec5c54651a9a2e53568f
Pierre Barre <pierre@barre.sh>
2025-11-28 15:28:44 +0100

    Add InodeStore abstraction for inode operations

--- zerofs/src/encryption.rs
+++ zerofs/src/encryption.rs
@@ -1,5 +1,5 @@
 use crate::fs::errors::FsError;
-use crate::fs::inode::{Inode, InodeId};
+use crate::fs::inode::InodeId;
 use crate::fs::key_codec::KeyCodec;
 use crate::fs::key_codec::KeyPrefix;
 use anyhow::Result;
@@ -122,30 +122,9 @@ impl EncryptedTransaction {
         self.inner.delete(key);
     }
 
-    pub fn save_inode(
-        &mut self,
-        id: InodeId,
-        inode: &Inode,
-    ) -> Result<(), Box<bincode::ErrorKind>> {
-        let key = KeyCodec::inode_key(id);
-        let data = bincode::serialize(inode)?;
-        self.put_bytes(&key, Bytes::from(data));
-        Ok(())
-    }
-
-    pub fn delete_inode(&mut self, id: InodeId) {
-        let key = KeyCodec::inode_key(id);
-        self.delete_bytes(&key);
-    }
-
     pub fn delete_chunk(&mut self, inode_id: InodeId, chunk_idx: u64) {
         let key = KeyCodec::chunk_key(inode_id, chunk_idx);
         self.delete_bytes(&key);
-    }
-
-    pub fn save_inode_counter(&mut self, next_id: u64) {
-        let key = KeyCodec::system_counter_key();
-        self.put_bytes(&key, KeyCodec::encode_counter(next_id));
     }
 
     pub fn add_tombstone(&mut self, inode_id: InodeId, size: u64) {
