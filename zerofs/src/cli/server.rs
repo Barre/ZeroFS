cf146532a526a139d51add542af0260ceb7dca1c
Pierre Barre <pierre@barre.sh>
2025-12-03 23:44:05 +0100

    Reduce GC interval to 1 min instead of 10 mins

--- zerofs/src/cli/server.rs
+++ zerofs/src/cli/server.rs
@@ -423,16 +423,16 @@ pub async fn build_slatedb(
         compression_codec: None, // Disable compression - we handle it in encryption layer
         garbage_collector_options: Some(GarbageCollectorOptions {
             wal_options: Some(GarbageCollectorDirectoryOptions {
-                interval: Some(Duration::from_mins(10)),
-                min_age: Duration::from_mins(10),
+                interval: Some(Duration::from_mins(1)),
+                min_age: Duration::from_mins(1),
             }),
             manifest_options: Some(GarbageCollectorDirectoryOptions {
-                interval: Some(Duration::from_mins(10)),
-                min_age: Duration::from_mins(10),
+                interval: Some(Duration::from_mins(1)),
+                min_age: Duration::from_mins(1),
             }),
             compacted_options: Some(GarbageCollectorDirectoryOptions {
-                interval: Some(Duration::from_mins(10)),
-                min_age: Duration::from_mins(10),
+                interval: Some(Duration::from_mins(1)),
+                min_age: Duration::from_mins(1),
             }),
         }),
         ..Default::default()
