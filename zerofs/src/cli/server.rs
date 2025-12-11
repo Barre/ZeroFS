f9ecb65ecb40cd525b84a5b02179720e57977071
Pierre Barre <pierre@barre.sh>
2025-12-11 15:35:17 +0100

    Increase max sst size

--- zerofs/src/cli/server.rs
+++ zerofs/src/cli/server.rs
@@ -412,7 +412,7 @@ pub async fn build_slatedb(
     let settings = slatedb::config::Settings {
         wal_enabled: false,
         l0_max_ssts,
-        l0_sst_size_bytes: 256 * 1024 * 1024,
+        l0_sst_size_bytes: 1024 * 1024 * 1024,
         filter_bits_per_key: 10,
         object_store_cache_options: ObjectStoreCacheOptions {
             root_folder: Some(cache_config.root_folder.clone()),
